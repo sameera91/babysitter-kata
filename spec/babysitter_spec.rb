2211e7abdadf824e1330406fa08412fe8a4a5d23
sameera91 <sameera.bachoti@gmail.com>
2017-01-09 20:04:39 -0500

    unit test for wage from 6pm - 10pm

--- spec/babysitter_spec.rb
+++ spec/babysitter_spec.rb
@@ -6,7 +6,7 @@ describe Babysitter do
     describe '#start_to_bedtime' do
       it 'returns the total wage from start to bedtime' do
         babysitter = Babysitter.new
-        expect(babysitter.start_to_bedtime()).to eq(36)
+        expect(babysitter.start_to_bedtime("6:00", "10:00")).to eq(36)
       end
     end
   end
