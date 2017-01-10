0a0b2a4b6a51cfc9f28b717de801c70987ca5b90
sameera91 <sameera.bachoti@gmail.com>
2017-01-10 17:55:21 -0500

    unit test to re-enter time

--- spec/babysitter_spec.rb
+++ spec/babysitter_spec.rb
@@ -78,7 +78,7 @@ describe Babysitter do
     end
 
     describe '#enter_hours' do 
-      it 'allows the user to enter the hours and displays the total wage' do
+      it 'allows the user to enter times and displays the total wage' do
         babysitter = Babysitter.new
         allow($stdout).to receive(:puts)
 
@@ -88,6 +88,21 @@ describe Babysitter do
 
         expect($stdout).to receive(:puts).with("Your total wage is $112")
 
+        babysitter.enter_hours
+      end
+
+      it 'allows the user to re-enter start time if invalid' do
+        babysitter = Babysitter.new
+        allow($stdout).to receive(:puts)
+        expect(babysitter).to receive(:gets).and_return("4:00")
+        expect($stdout).to receive(:puts).with("Must be a time between 5pm and 12am. Try again.")
+        expect(babysitter).to receive(:gets).and_return("6:30")
+        expect($stdout).to receive(:puts).with("Must be a full hour. Try again.")
+        expect(babysitter).to receive(:gets).and_return("6:00")
+        expect(babysitter).to receive(:gets).and_return("10:00")
+        expect(babysitter).to receive(:gets).and_return("3:00")
+
+        expect($stdout).to receive(:puts).with("Your total wage is $112")
         babysitter.enter_hours
       end
     end
