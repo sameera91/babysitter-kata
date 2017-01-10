eb2d9ca7a827f6e65d779629d523aa67b34cf1a7
sameera91 <sameera.bachoti@gmail.com>
2017-01-09 20:45:04 -0500

    total wage unit test passing

--- lib/babysitter.rb
+++ lib/babysitter.rb
@@ -2,25 +2,36 @@ class Babysitter
 
   MIDNIGHT = 12
 
+  def initialize
+    @wage1 = 0
+    @wage2 = 0
+    @wage3 = 0
+  end
+
   def start_to_bedtime(start_time, bedtime)
     start_time = start_time.match(/([^:]+)/)[0].to_i
     bedtime = bedtime.match(/([^:]+)/)[0].to_i
     hours = bedtime - start_time
-    wage = 12 * hours
-    return wage
+    @wage1 = 12 * hours
+    return @wage1
   end
 
   def bedtime_to_midnight(bedtime)
     bedtime = bedtime.match(/([^:]+)/)[0].to_i
     hours = MIDNIGHT - bedtime
-    wage = 8 * hours
-    return wage
+    @wage2 = 8 * hours
+    return @wage2
   end
 
   def midnight_to_end(endtime)
     endtime = endtime.match(/([^:]+)/)[0].to_i
     hours = endtime
-    wage = 16 * hours
-    return wage
+    @wage3 = 16 * hours
+    return @wage3
+  end
+
+  def add_total
+    total_wage = @wage1 + @wage2 + @wage3
+    return total_wage
   end
 end
