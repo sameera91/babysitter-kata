13030a027b9cbf6d3a69cbe4b23394b99bee6e96
sameera91 <sameera.bachoti@gmail.com>
2017-01-09 20:16:21 -0500

    bedtime to midnight unit test passing

--- lib/babysitter.rb
+++ lib/babysitter.rb
@@ -1,9 +1,20 @@
 class Babysitter
+
+  MIDNIGHT = 12
+
   def start_to_bedtime(start_time, bedtime)
     start_time = start_time.match(/([^:]+)/)[0].to_i
     bedtime = bedtime.match(/([^:]+)/)[0].to_i
-    hours = bedtime- start_time
+    hours = bedtime - start_time
     wage = 12 * hours
     return wage
   end
+
+  def bedtime_to_midnight(bedtime)
+    bedtime = bedtime.match(/([^:]+)/)[0].to_i
+    hours = MIDNIGHT - bedtime
+    wage = 8 * hours
+    return wage
+  end
+
 end
