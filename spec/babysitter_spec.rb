aeb99c8751a9518d31dcd0d78c2adafd72fad4cc
sameera91 <sameera.bachoti@gmail.com>
2017-01-09 20:13:17 -0500

    unit test for bedtime to midnight

--- spec/babysitter_spec.rb
+++ spec/babysitter_spec.rb
@@ -9,5 +9,12 @@ describe Babysitter do
         expect(babysitter.start_to_bedtime("6:00", "10:00")).to eq(48)
       end
     end
+
+    describe '#bedtime_to_midnight' do
+      it 'returns the total wage from bedtime to midnight' do
+        babysitter = Babysitter.new
+        expect(babysitter.bedtime_to_midnight("10:00")).to eq(16)
+      end
+    end
   end
 end
