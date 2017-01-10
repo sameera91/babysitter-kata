99b961c99aa7340c53a4628b0125fbfc41b0ed25
sameera91 <sameera.bachoti@gmail.com>
2017-01-09 20:09:15 -0500

    write method to pass first unit test

--- lib/babysitter.rb
+++ lib/babysitter.rb
@@ -1,5 +1,9 @@
 class Babysitter
-  def start_to_bedtime
-    return 36
+  def start_to_bedtime(start_time, bedtime)
+    start_time = start_time.match(/([^:]+)/)[0].to_i
+    bedtime = bedtime.match(/([^:]+)/)[0].to_i
+    hours = bedtime- start_time
+    wage = 12 * hours
+    return wage
   end
 end
