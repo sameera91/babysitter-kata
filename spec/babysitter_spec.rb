17b109b1c43d1fe86c037c88a85ef579392d846b
sameera91 <sameera.bachoti@gmail.com>
2017-01-09 20:44:18 -0500

    total wage unit test

--- spec/babysitter_spec.rb
+++ spec/babysitter_spec.rb
@@ -23,5 +23,16 @@ describe Babysitter do
         expect(babysitter.midnight_to_end("3:00")).to eq(48)
       end
     end
+
+    describe '#add_total' do 
+      it 'returns the total wage from start to end' do 
+        babysitter = Babysitter.new
+        wage1 = babysitter.start_to_bedtime("6:00", "10:00")
+        wage2 = babysitter.bedtime_to_midnight("10:00")
+        wage3 = babysitter.midnight_to_end("3:00")
+        total_wage = wage1 + wage2 + wage3
+        expect(babysitter.add_total()).to eq(total_wage)
+      end
+    end
   end
 end
