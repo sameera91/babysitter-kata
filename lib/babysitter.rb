6918db312b086a766d9a7b303077b8ada624b03c
sameera91 <sameera.bachoti@gmail.com>
2017-01-10 15:58:07 -0500

    valid times unit tests passing

--- lib/babysitter.rb
+++ lib/babysitter.rb
@@ -10,11 +10,11 @@ class Babysitter
   end
 
   def enter_hours
-    puts "Please enter start time"
+    puts "Please enter start time between 5pm and 12am"
     start_time = gets.strip
-    puts "Please enter bedtime"
+    puts "Please enter a bedtime between 6pm and 11pm"
     bedtime = gets.strip
-    puts "Please enter endtime"
+    puts "Please enter an endtime between 12am and 4pm"
     endtime = gets.strip
 
     start_to_bedtime(start_time, bedtime)
@@ -24,6 +24,44 @@ class Babysitter
 
     puts "Your total wage is $" + @total_wage.to_s
   end
+
+  def valid_time(time)
+    minutes = time.match('\:(.*)')[1].to_i
+    hour = time.match(/([^:]+)/)[0].to_i
+    valid = true
+    if minutes != 00 || hour > 12 || hour < 1
+      valid = false
+    end
+    return valid
+  end
+
+  def valid_start_time(time)
+    hour = time.match(/([^:]+)/)[0].to_i
+    valid = true
+    if hour < 5 || hour > 11
+      valid = false
+    end
+    return valid
+  end
+
+  def valid_bed_time(time)
+    hour = time.match(/([^:]+)/)[0].to_i
+    valid = true
+    if hour < 6 || hour > 11
+      valid = false
+    end
+    return valid
+  end
+
+  def valid_end_time(time)
+    hour = time.match(/([^:]+)/)[0].to_i
+    valid = true
+    if hour > 12 || (hour >= 5 && hour < 11)
+      valid = false
+    end
+    return valid
+  end
+
 
   def start_to_bedtime(start_time, bedtime)
     start_time = start_time.match(/([^:]+)/)[0].to_i
